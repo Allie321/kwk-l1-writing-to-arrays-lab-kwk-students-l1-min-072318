ac11f47c8b9b3b2a3acc78e94d1a4e322b7ecc74
Allie321 <allisonroberson04@gmail.com>
2018-07-25 15:39:49 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,11 +8,13 @@ def change_rainbow_colors
   RAINBOW_COLORS[0] = "red"
   RAINBOW_COLORS[1] = "light_red"
   RAINBOW_COLORS[2] = "light yellow"
+  return RAINBOW_COLORS
 end
 puts change_rainbow_colors
 
 def add_colors
  RAINBOW_COLORS << "green"
  RAINBOW_COLORS << "blue"
+ return RAINBOW_COLORS
  end
- puts add_colors
+puts add_colors
